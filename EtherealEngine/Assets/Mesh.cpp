969ce51df17e1d0983d339e2898d80907c6c7046
Matt B <cronos1979@live.com>
2025-06-22 09:03:18 -0500

    Bug fix

--- EtherealEngine/Assets/Mesh.cpp
+++ EtherealEngine/Assets/Mesh.cpp
@@ -11,6 +11,7 @@ namespace Ethereal
 		m_Indices(indices),
 		m_MaterialIndex(materialIndex)
 	{
+		Initialize(vertices, indices);
 	}
 
 	bool Mesh::Initialize(const std::vector<Vertex>& vertices, const std::vector<uint32_t>& indices)
