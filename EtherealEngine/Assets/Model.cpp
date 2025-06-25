cc7427bd50f28dfbbb6812afdab5cd4417a6595c
Matt B <cronos1979@live.com>
2025-06-25 04:46:51 -0500

    Working on Model to math Material

--- EtherealEngine/Assets/Model.cpp
+++ EtherealEngine/Assets/Model.cpp
@@ -1,9 +1,7 @@
-#include "Model.h"
+#include "Assets/Model.h"
 #include "Core/Logger.h"
 #include "Core/EEContext.h"
 #include "Renderer/DX11/VertexBufferLayout.h"
-
-// Assimp
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
@@ -22,8 +20,7 @@ namespace Ethereal
 			path,
 			aiProcess_Triangulate |
 			aiProcess_JoinIdenticalVertices |
-			aiProcess_ConvertToLeftHanded
-		);
+			aiProcess_ConvertToLeftHanded);
 
 		if (!scene || !scene->HasMeshes())
 		{
@@ -31,7 +28,6 @@ namespace Ethereal
 			return false;
 		}
 
-		// Load Materials
 		for (unsigned int i = 0; i < scene->mNumMaterials; ++i)
 		{
 			const aiMaterial* aiMat = scene->mMaterials[i];
@@ -47,11 +43,16 @@ namespace Ethereal
 			auto ovr = m_MaterialOverrides.find(i);
 			if (ovr != m_MaterialOverrides.end())
 			{
-				material->SetVertexShaderName(ovr->second.vertexShader);
-				material->SetPixelShaderName(ovr->second.pixelShader);
-				material->SetDiffuseTexturePath(ovr->second.diffuseTexture);
-				material->SetNormalTexturePath(ovr->second.normalTexture);
-				material->SetInputLayoutJson(ovr->second.inputLayout);
+				const MaterialOverride & override = ovr->second;
+				material->SetVertexShaderName(override.vertexShader);
+				material->SetPixelShaderName(override.pixelShader);
+				material->SetDiffuseTexturePath(override.diffuseTexture);
+				material->SetNormalTexturePath(override.normalTexture);
+				material->SetSpecularTexturePath(override.specularTexture);
+				material->SetMetallicTexturePath(override.metallicTexture);
+				material->SetRoughnessTexturePath(override.roughnessTexture);
+				material->SetAOTexturePath(override.aoTexture);
+				material->SetInputLayoutJson(override.inputLayout);
 			}
 			else
 			{
@@ -69,7 +70,6 @@ namespace Ethereal
 			m_Materials.push_back(material);
 		}
 
-		// Load Meshes
 		for (unsigned int m = 0; m < scene->mNumMeshes; ++m)
 		{
 			const aiMesh* assimpMesh = scene->mMeshes[m];
@@ -88,7 +88,6 @@ namespace Ethereal
 				layout.AddAttribute(VertexAttributeType::TexCoord, DXGI_FORMAT_R32G32_FLOAT, "TEXCOORD");
 			if (hasTangents)
 				layout.AddAttribute(VertexAttributeType::Tangent, DXGI_FORMAT_R32G32B32_FLOAT, "TANGENT");
-
 			layout.Build();
 			UINT stride = layout.GetStride();
 
@@ -96,8 +95,6 @@ namespace Ethereal
 			for (unsigned int i = 0; i < assimpMesh->mNumVertices; ++i)
 			{
 				uint8_t* dst = vertexData.data() + i * stride;
-
-				// Position
 				memcpy(dst, &assimpMesh->mVertices[i], sizeof(float) * 3);
 				UINT offset = sizeof(float) * 3;
 
@@ -134,9 +131,6 @@ namespace Ethereal
 
 			mesh->Initialize(std::move(vertexData), std::move(layout), std::move(indices));
 			m_Meshes.push_back(mesh);
-
-			/*LOG_INFO("Loaded mesh '{}' with {} verts, {} indices, material {}",
-				mesh->GetName(), assimpMesh->mNumVertices, indices.size(), mesh->GetMaterialIndex());*/
 		}
 
 		if (!CreateConstantBuffer())
@@ -147,6 +141,46 @@ namespace Ethereal
 
 		LOG_INFO("Model '{}' loaded with {} mesh(es)", path, m_Meshes.size());
 		return true;
+	}
+
+	ConstantBuffer<CB_VS_vertexshader>& Model::GetConstantBuffer()
+	{
+		return m_ConstantBuffer;
+	}
+
+	void Model::SetMaterialOverride(int materialIndex, const MaterialOverride & override)
+	{
+		m_MaterialOverrides[materialIndex] = override;
+	}
+
+	void Model::SetMaterials(std::vector<std::shared_ptr<Material>>&& materials)
+	{
+		m_Materials = std::move(materials);
+	}
+
+	const std::vector<std::shared_ptr<Material>>& Model::GetMaterials() const
+	{
+		return m_Materials;
+	}
+
+	std::shared_ptr<Ethereal::Material> Model::GetMaterial(size_t index) const
+	{
+		return (index < m_Materials.size()) ? m_Materials[index] : nullptr;
+	}
+
+	void Model::SetMeshes(std::vector<std::shared_ptr<Mesh>>&& meshes)
+	{
+		m_Meshes = std::move(meshes);
+	}
+
+	const std::vector<std::shared_ptr<Mesh>>& Model::GetMeshes() const
+	{
+		return m_Meshes;
+	}
+
+	std::shared_ptr<Mesh> Model::GetMesh(size_t index) const
+	{
+		return (index < m_Meshes.size()) ? m_Meshes[index] : nullptr;
 	}
 
 	bool Model::CreateConstantBuffer()
@@ -161,4 +195,168 @@ namespace Ethereal
 		}
 		return true;
 	}
-}
+} // namespace Ethereal
+// #include "Model.h"
+// #include "Core/Logger.h"
+// #include "Core/EEContext.h"
+// #include "Renderer/DX11/VertexBufferLayout.h"
+// 
+// // Assimp
+// #include <assimp/Importer.hpp>
+// #include <assimp/scene.h>
+// #include <assimp/postprocess.h>
+// 
+// namespace Ethereal
+// {
+// 	bool Model::LoadFromFile(const std::string& path)
+// 	{
+// 		auto assetManager = EEContext::Get().GetAssetManager();
+// 		ID3D11Device* device = static_cast<ID3D11Device*>(EEContext::Get().GetDevice());
+// 		m_Meshes.clear();
+// 		m_Materials.clear();
+// 
+// 		Assimp::Importer importer;
+// 		const aiScene* scene = importer.ReadFile(
+// 			path,
+// 			aiProcess_Triangulate |
+// 			aiProcess_JoinIdenticalVertices |
+// 			aiProcess_ConvertToLeftHanded
+// 		);
+// 
+// 		if (!scene || !scene->HasMeshes())
+// 		{
+// 			LOG_ERROR("Failed to load model: {}", path);
+// 			return false;
+// 		}
+// 
+// 		// Load Materials
+// 		for (unsigned int i = 0; i < scene->mNumMaterials; ++i)
+// 		{
+// 			const aiMaterial* aiMat = scene->mMaterials[i];
+// 			auto material = std::make_shared<Material>();
+// 			material->GetConstantBuffer().Initialize(device, nullptr);
+// 
+// 			aiColor3D color(1.0f, 1.0f, 1.0f);
+// 			if (AI_SUCCESS == aiMat->Get(AI_MATKEY_COLOR_DIFFUSE, color))
+// 			{
+// 				material->SetDiffuseColor({ color.r, color.g, color.b });
+// 			}
+// 
+// 			auto ovr = m_MaterialOverrides.find(i);
+// 			if (ovr != m_MaterialOverrides.end())
+// 			{
+// 				material->SetVertexShaderName(ovr->second.vertexShader);
+// 				material->SetPixelShaderName(ovr->second.pixelShader);
+// 				material->SetDiffuseTexturePath(ovr->second.diffuseTexture);
+// 				material->SetNormalTexturePath(ovr->second.normalTexture);
+// 				material->SetInputLayoutJson(ovr->second.inputLayout);
+// 			}
+// 			else
+// 			{
+// 				material->SetVertexShaderName(m_VertexShaderName);
+// 				material->SetPixelShaderName(m_PixelShaderName);
+// 				material->SetDiffuseTexturePath(m_DiffuseTexturePath);
+// 				material->SetNormalTexturePath(m_NormalTexturePath);
+// 			}
+// 
+// 			if (!material->Initialize())
+// 			{
+// 				LOG_ERROR("Failed to initialize material for model: {}", path);
+// 				return false;
+// 			}
+// 			m_Materials.push_back(material);
+// 		}
+// 
+// 		// Load Meshes
+// 		for (unsigned int m = 0; m < scene->mNumMeshes; ++m)
+// 		{
+// 			const aiMesh* assimpMesh = scene->mMeshes[m];
+// 			auto mesh = std::make_shared<Mesh>();
+// 			mesh->SetName(assimpMesh->mName.C_Str());
+// 			mesh->SetMaterialIndex(assimpMesh->mMaterialIndex);
+// 
+// 			VertexBufferLayout layout;
+// 			layout.AddAttribute(VertexAttributeType::Position, DXGI_FORMAT_R32G32B32_FLOAT, "POSITION");
+// 			bool hasNormals = assimpMesh->HasNormals();
+// 			bool hasTexCoords = assimpMesh->HasTextureCoords(0);
+// 			bool hasTangents = assimpMesh->HasTangentsAndBitangents();
+// 			if (hasNormals)
+// 				layout.AddAttribute(VertexAttributeType::Normal, DXGI_FORMAT_R32G32B32_FLOAT, "NORMAL");
+// 			if (hasTexCoords)
+// 				layout.AddAttribute(VertexAttributeType::TexCoord, DXGI_FORMAT_R32G32_FLOAT, "TEXCOORD");
+// 			if (hasTangents)
+// 				layout.AddAttribute(VertexAttributeType::Tangent, DXGI_FORMAT_R32G32B32_FLOAT, "TANGENT");
+// 
+// 			layout.Build();
+// 			UINT stride = layout.GetStride();
+// 
+// 			std::vector<uint8_t> vertexData(assimpMesh->mNumVertices * stride);
+// 			for (unsigned int i = 0; i < assimpMesh->mNumVertices; ++i)
+// 			{
+// 				uint8_t* dst = vertexData.data() + i * stride;
+// 
+// 				// Position
+// 				memcpy(dst, &assimpMesh->mVertices[i], sizeof(float) * 3);
+// 				UINT offset = sizeof(float) * 3;
+// 
+// 				if (hasNormals)
+// 				{
+// 					memcpy(dst + offset, &assimpMesh->mNormals[i], sizeof(float) * 3);
+// 					offset += sizeof(float) * 3;
+// 				}
+// 
+// 				if (hasTexCoords)
+// 				{
+// 					float uv[2] = {
+// 						assimpMesh->mTextureCoords[0][i].x,
+// 						assimpMesh->mTextureCoords[0][i].y
+// 					};
+// 					memcpy(dst + offset, uv, sizeof(float) * 2);
+// 					offset += sizeof(float) * 2;
+// 				}
+// 
+// 				if (hasTangents)
+// 				{
+// 					memcpy(dst + offset, &assimpMesh->mTangents[i], sizeof(float) * 3);
+// 					offset += sizeof(float) * 3;
+// 				}
+// 			}
+// 
+// 			std::vector<uint32_t> indices;
+// 			for (unsigned int i = 0; i < assimpMesh->mNumFaces; ++i)
+// 			{
+// 				const aiFace& face = assimpMesh->mFaces[i];
+// 				for (unsigned int j = 0; j < face.mNumIndices; ++j)
+// 					indices.push_back(face.mIndices[j]);
+// 			}
+// 
+// 			mesh->Initialize(std::move(vertexData), std::move(layout), std::move(indices));
+// 			m_Meshes.push_back(mesh);
+// 
+// 			/*LOG_INFO("Loaded mesh '{}' with {} verts, {} indices, material {}",
+// 				mesh->GetName(), assimpMesh->mNumVertices, indices.size(), mesh->GetMaterialIndex());*/
+// 		}
+// 
+// 		if (!CreateConstantBuffer())
+// 		{
+// 			LOG_ERROR("Failed to create constant buffer for model: {}", path);
+// 			return false;
+// 		}
+// 
+// 		LOG_INFO("Model '{}' loaded with {} mesh(es)", path, m_Meshes.size());
+// 		return true;
+// 	}
+// 
+// 	bool Model::CreateConstantBuffer()
+// 	{
+// 		ID3D11Device* device = static_cast<ID3D11Device*>(EEContext::Get().GetDevice());
+// 		ID3D11DeviceContext* context = static_cast<ID3D11DeviceContext*>(EEContext::Get().GetContext());
+// 		HRESULT hr = m_ConstantBuffer.Initialize(device, context);
+// 		if (FAILED(hr))
+// 		{
+// 			LOG_ERROR("Failed to initialize model constant buffer: {}", hr);
+// 			return false;
+// 		}
+// 		return true;
+// 	}
+// }
--- EtherealEngine/Assets/Model.h
+++ EtherealEngine/Assets/Model.h
@@ -2,6 +2,7 @@
 #include "IAsset.h"
 #include "Core/EtherealIncludes.h"
 #include <vector>
+#include <unordered_map>
 #include "Assets/Mesh.h"
 #include "Assets/Material.h"
 #include "Renderer/DX11/ConstantBuffer.h"
@@ -16,71 +17,33 @@ namespace Ethereal
 		virtual ~Model() = default;
 
 		bool LoadFromFile(const std::string& path);
+		ConstantBuffer<CB_VS_vertexshader>& GetConstantBuffer();
 
-		ConstantBuffer<CB_VS_vertexshader>& GetConstantBuffer()
-		{
-			return m_ConstantBuffer;
-		}
+		void SetMaterials(std::vector<std::shared_ptr<Material>>&& materials);
+		const std::vector<std::shared_ptr<Material>>& GetMaterials() const;
+		std::shared_ptr<Material> GetMaterial(size_t index) const;
 
-		void SetMaterials(std::vector<std::shared_ptr<Material>>&& materials)
-		{
-			m_Materials = std::move(materials);
-		}
+		void SetMeshes(std::vector<std::shared_ptr<Mesh>>&& meshes);
+		const std::vector<std::shared_ptr<Mesh>>& GetMeshes() const;
+		std::shared_ptr<Mesh> GetMesh(size_t index) const;
 
-		const std::vector<std::shared_ptr<Material>>& GetMaterials() const
-		{
-			return m_Materials;
-		}
-
-		std::shared_ptr<Material> GetMaterial(size_t index) const
-		{
-			return (index < m_Materials.size()) ? m_Materials[index] : nullptr;
-		}
-
-		void SetMeshes(std::vector<std::shared_ptr<Mesh>>&& meshes)
-		{
-			m_Meshes = std::move(meshes);
-		}
-
-		const std::vector<std::shared_ptr<Mesh>>& GetMeshes() const
-		{
-			return m_Meshes;
-		}
-
-		std::shared_ptr<Mesh> GetMesh(size_t index) const
-		{
-			return (index < m_Meshes.size()) ? m_Meshes[index] : nullptr;
-		}
-
-		void SetVertexShaderName(const std::string& name)
-		{
-			m_VertexShaderName = name;
-		}
-		void SetPixelShaderName(const std::string& name)
-		{
-			m_PixelShaderName = name;
-		}
-		void SetDiffuseTexturePath(const std::string& path)
-		{
-			m_DiffuseTexturePath = path;
-		}
-
-		void SetMaterialOverride(int materialIndex, const MaterialOverride & override)
-		{
-			m_MaterialOverrides[materialIndex] = override;
-		}
+		void SetVertexShaderName(const std::string& name);
+		void SetPixelShaderName(const std::string& name);
+		void SetDiffuseTexturePath(const std::string& path);
+		void SetNormalTexturePath(const std::string& path);
+		void SetMaterialOverride(int materialIndex, const MaterialOverride & override);
 
 	private:
 		bool CreateConstantBuffer();
 
 		std::vector<std::shared_ptr<Material>> m_Materials;
 		std::vector<std::shared_ptr<Mesh>> m_Meshes;
+		std::unordered_map<int, MaterialOverride> m_MaterialOverrides;
 
 		ConstantBuffer<CB_VS_vertexshader> m_ConstantBuffer;
 		std::string m_VertexShaderName = "VertexShader";
 		std::string m_PixelShaderName = "PixelShader";
 		std::string m_DiffuseTexturePath = "Textures/Test.png";
 		std::string m_NormalTexturePath = "Textures/Normal.png";
-		std::unordered_map<int, MaterialOverride> m_MaterialOverrides;
 	};
 }
