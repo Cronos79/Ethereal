dd366108edd1adda4334537002621ba2e56cdcd5
Matt B <cronos1979@live.com>
2025-06-26 06:53:12 -0500

    Working on shaders

---
+++ KingdomOfEnoch/Game/CubeRingObj.cpp
@@ -0,0 +1,20 @@
+#include "CubeRingObj.h"
+#include "Assets/AssetFactory.h"
+
+REGISTER_GAMEOBJECT_TYPE(CubeRingObj);  // Enables factory-based creation from JSON/type strings
+
+CubeRingObj::CubeRingObj()
+	: GameObject("CubeRingObj")
+{
+
+}
+
+void CubeRingObj::Update(float deltaTime)
+{
+
+}
+
+void CubeRingObj::DrawUI(float deltaTime)
+{
+
+}
--- KingdomOfEnoch/Game/TestScene.cpp
+++ KingdomOfEnoch/Game/TestScene.cpp
@@ -17,7 +17,12 @@ TestScene::TestScene(const std::string& name)
 	result = assetManager.LoadGameObject("GO_SmallCone");
 	if (!result)
 	{
-		LOG_WARN("Failed to load GameObject 'TestCube' in TestScene.");
+		LOG_WARN("Failed to load GameObject 'SmallCone' in TestScene.");
+	}
+	result = assetManager.LoadGameObject("GO_CubeRing");
+	if (!result)
+	{
+		LOG_WARN("Failed to load GameObject 'CubeRing' in TestScene.");
 	}
 	result = assetManager.LoadGameObject("MainLight");
 	if (!result)
@@ -30,14 +35,17 @@ TestScene::TestScene(const std::string& name)
 		auto light = assetManager.Get<Ethereal::GameObject>("MainLight");
 		auto lightclone = light->Clone();
 		AddGameObject(lightclone);
-		auto originalCube = assetManager.Get<Ethereal::GameObject>("TestCube");
-		auto originalCone = assetManager.Get<Ethereal::GameObject>("GO_SmallCone");
+		auto cubeRing = assetManager.Get<Ethereal::GameObject>("GO_CubeRing");
+		auto cubeRingclone = cubeRing->Clone();
+		AddGameObject(cubeRingclone);
+	/*	auto originalCube = assetManager.Get<Ethereal::GameObject>("TestCube");
+		auto originalCone = assetManager.Get<Ethereal::GameObject>("GO_SmallCone");*/
 		for (int i = 0; i < 100; i++)
 		{
-			auto clone = originalCube->Clone();
+		/*	auto clone = originalCube->Clone();
 			AddGameObject(clone);
 			auto coneClone = originalCone->Clone();
-			AddGameObject(coneClone);
+			AddGameObject(coneClone);*/
 		}
 	}
 	else
